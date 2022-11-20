a42f14314c8f10592e603f664a8161f16755b964
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-11-20 20:05:05 +0100

    not done

--- src/components/RandomBeer.jsx
+++ src/components/RandomBeer.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
-function RandomBeer() {
+function RandomBeer(props) {
+
+  const [randomBeer, setRandomBeer] = useState([]);
+
+  useEffect (() => {
+    const fetchBeers = async () => {
+      try {
+        let response = await axios.get("https://ih-beers-api2.herokuapp.com/beers")
+      console.log(response.data)
+      setRandomBeer(response.data)   
+      }
+      catch(err) {
+        console.log(err)
+      }   
+    }
+      fetchBeers()
+  }, []);
+  
   return (
+
     <>
     <Link to="/"> Home </Link> 
+    <h1>Random Beer</h1>  
+   
+
+      
+        return (
+
+        <div key={randomBeer._id}>
+        <img src={randomBeer.image_url} height={300} alt={randomBeer.name.toLowerCase()} />
+        <h2>{randomBeer.name}</h2>
+        <h3>{randomBeer.tagline}</h3>
+        <p>{randomBeer.first_brewed}</p>
+        <p>{randomBeer.attenuation_level}</p>
+        <p>{randomBeer.description}</p>
+        <p>{randomBeer.contributed_by}</p>
+        </div>
+        )
+        
+    
    </>
+   
   )
 }
 
